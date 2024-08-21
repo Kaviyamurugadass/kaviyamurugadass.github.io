a6ac4bd0589abd60d48c6665b0ca4e6b1a378842
Kaviya <131757368+Kaviyamurugadass@users.noreply.github.com>
2024-08-21 11:39:28 +0530

    commit

--- HW/YES!NO/src/components/MyHeader.jsx
+++
@@ -1,9 +0,0 @@
-export default function MyHeader(){
-    return(
-<div className="w3-display-topmiddle ">
-        <h1 style={{color:'black',padding:15,fontSize:60,textShadow:'2px 4px 2px black',fontWeight:600}}><span style={{color: '#00ff00'}}>YES</span>/<span style={{color: '#de1212'}}>NO</span>
-        </h1>
-    <main style={{fontWeight:500,fontSize:15,fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"}}>
-        "Are you feeling too confused to make a decision? Let's spin the wheel and let it make the decision for you."</main>
-</div>);
-}
--- HW/YES!NO/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-
-import App from "./App.jsx";
-import "./style.css";
-
-const entry = document.getElementById("root");
-ReactDOM.createRoot(entry).render(<App />);
